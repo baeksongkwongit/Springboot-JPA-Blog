a7a1b2eac4dddae01c2e4eeafaf5ffd34181597f
baeksong.kwon <baeksong.kwongit@gmail.com>
2023-01-29 03:29:32 +0900

    환경세팅완료v1

---
+++ README.md
@@ -0,0 +1,1 @@
+# Srpingboot-JPA-Bloe
---
+++ src/main/java/com/cos/blog/Blog1Application.java
@@ -0,0 +1,15 @@
+package com.cos.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blog1Application {
+
+	public static void main(String[] args) {
+		System.setProperty("org.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH", "true");
+
+		SpringApplication.run(Blog1Application.class, args);
+	}
+
+}
